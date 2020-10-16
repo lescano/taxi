6a5268bab00de2037d372adfaeee85f2542d7b06
lescano <lescanov24@gmail.com>
2020-10-16 15:11:49 -0300

    Trabajo de Emiliano y Jorge mas el nitro del taxi

--- Assets/Scenes/Scripts/ControlesTaxi.cs
+++ Assets/Scenes/Scripts/ControlesTaxi.cs
@@ -1,54 +1,115 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlesTaxi : MonoBehaviour
 {
-    float velMaxima = 10f;// Máxima velocidad que puede alcanzar el auto al acelerar
-    float aceleracion = 7f;// Indice que indica cuanto se tiene que mover el auto 
-    float velRotacion = 2f; //Velocidad de rotacion 
-    
+    //Velocidades del auto
+    public float velMaxima = 7f;// Máxima velocidad que puede alcanzar el auto al acelerar
+    private float aceleracion = 4f;// Indice que indica cuanto se tiene que mover el auto 
+    private float velRotacion = 0.5f; //Velocidad de rotacion 
+
+    //Datos habilidad nitro
+    public float tiempoNitro = 10.0f; //tiempo que está activado el nitro
+    private bool activarNitro = false;
+    public Text tiempo_nitro;
+    public Text mensaje_nitro;
+    public float tiempoEsperaNitro = 30.0f;
+    private bool activarTiempoEsperaNitro = false;
+
+
     Rigidbody2D rb;
 
     float x;// para girar el auto a la derecha o a la izquierda
     float y;// para controlar la velocidad del auto (acelerar o frenar)
 
     void Start(){
-    	rb = GetComponent<Rigidbody2D>();
-
+        rb = GetComponent<Rigidbody2D>();
+        //tiempo_nitro = GameObject.Find("Canvas/Tiempo_nitro2").GetComponent<UnityEngine.UI.Text>();
+        //mensaje_nitro = GameObject.Find("Canvas/Tiempo_nitro").GetComponent<UnityEngine.UI.Text>();
     }
 
     void Update(){
 
-    	x = Input.GetAxis("Horizontal");
-    	y = Input.GetAxis("Vertical");
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
 
-    	Vector2 movimiento = transform.up * (y * aceleracion);
-    	rb.AddForce(movimiento);
+        Vector2 movimiento = transform.up * (y * aceleracion);
+        rb.AddForce(movimiento);
 
-    	float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
+        float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
 
-    	if (aceleracion > 0){
-    		if(direction > 0){
-    			rb.rotation -= x * velRotacion * (rb.velocity.magnitude / velMaxima);
-    		}
-    		else{
-    			rb.rotation += x * velRotacion * (rb.velocity.magnitude / velMaxima);
-    		}
-    	}
+        if (aceleracion > 0){
+            if(direction > 0){
+                rb.rotation -= x * velRotacion * (rb.velocity.magnitude / velMaxima);
+            }
+            else{
+                rb.rotation += x * velRotacion * (rb.velocity.magnitude / velMaxima);
+            }
+        }
 
-    	float driftForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.left)) * 2.0f;
+        float driftForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.left)) * 2.0f;
 
-    	Vector2 relativeForce = Vector2.right * driftForce;
+        Vector2 relativeForce = Vector2.right * driftForce;
 
-    	rb.AddForce(rb.GetRelativeVector(relativeForce));
+        rb.AddForce(rb.GetRelativeVector(relativeForce));
 
-    	if(rb.velocity.magnitude > velMaxima){
-    		rb.velocity = rb.velocity.normalized * velMaxima;
-    	}
+        if(rb.velocity.magnitude > velMaxima){
+            rb.velocity = rb.velocity.normalized * velMaxima;
+        }
 
-    	//Debug.DrawLine(rb.position, rb.GetRelativePoint(relativeForce), Color.green);
+        if (Input.GetKey("z")){
+            if (!activarNitro && tiempoEsperaNitro==30.0f){
+                Nitro();
+            }
+
+        }
+
+        if(activarNitro){
+            //tiempo_nitro.text = tiempoNitro.ToString("f0");
+            tiempoNitro -= Time.deltaTime;
+        }
+        
+
+        if (activarTiempoEsperaNitro){
+
+            //mensaje_nitro.text = "Nitro en "+tiempoEsperaNitro.ToString("f0");
+            //tiempo_nitro.text = "";
+            tiempoEsperaNitro -= Time.deltaTime;
+        }
+
+        if(activarNitro&&tiempoNitro<= 0.0f){
+            
+            velMaxima = velMaxima / 2;
+            aceleracion = aceleracion / 2;
+            velRotacion = velRotacion / 2;
+
+            activarNitro = false;
+            tiempoNitro = 10.0f;
+            //tiempo_nitro.text = "--";
+            activarTiempoEsperaNitro = true;
+        }
+
+        if (tiempoEsperaNitro<= 0.0f){
+            activarTiempoEsperaNitro = false;
+            tiempoEsperaNitro = 30.0f;
+            //mensaje_nitro.text = "Nitro:";
+            //tiempo_nitro.text = "--";
+        }
+
     }
+
+    public void Nitro(){
+        if ( activarNitro == false){
+            activarNitro = true;
+
+            velMaxima = velMaxima * 2;
+            aceleracion = aceleracion * 2;
+            velRotacion = velRotacion * 2;
+        }
+    }
+
 }
 
 
---
+++ Assets/Scenes/Scripts/camara.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class camara : MonoBehaviour
+{
+
+    public GameObject personaje;
+    private Vector3 posicion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicion = transform.position - personaje.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = personaje.transform.position + posicion;
+    }
+
+}
